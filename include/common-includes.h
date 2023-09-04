c02e6ae403b21c29c352a07c32fb77ecadb0adb6
Rahul Anwardeen <ranwardeen@uhnder.com>
2023-09-04 12:35:41 +0530

    Add additional include

--- include/common-includes.h
+++ include/common-includes.h
@@ -1,11 +1,12 @@
 #ifndef COMMON_INCLUDES_H
 #define COMMON_INCLUDES_H
 #include <opencv4/opencv2/core.hpp>
+#include <opencv4/opencv2/highgui.hpp>
 #include <opencv4/opencv2/features2d.hpp>
 #include <iostream>
 #include <cstdio>
 #include <string>
 #include <vector>
 using namespace std;
-
+using namespace cv;
 #endif //COMMON_INCLUDES_H
