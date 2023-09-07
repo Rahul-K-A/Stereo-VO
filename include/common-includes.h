2f2b45b503a211cd6909c32cb8017266d0e35d2f
Rahul Anwardeen <ranwardeen@uhnder.com>
2023-09-07 10:26:35 +0530

    Switch back to opencv3

--- include/common-includes.h
+++ include/common-includes.h
@@ -1,8 +1,11 @@
 #ifndef COMMON_INCLUDES_H
 #define COMMON_INCLUDES_H
-#include <opencv4/opencv2/core.hpp>
-#include <opencv4/opencv2/highgui.hpp>
-#include <opencv4/opencv2/features2d.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/features2d.hpp>
+#include <opencv2/calib3d.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/ximgproc/disparity_filter.hpp>
 #include <iostream>
 #include <cstdio>
 #include <string>
