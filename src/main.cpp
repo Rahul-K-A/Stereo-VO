1f470a730319a7abf814576bd40a00f055f01aa7
Rahul Anwardeen <ranwardeen@uhnder.com>
2023-09-18 03:13:00 +0530

    Golden run

--- src/main.cpp
+++ src/main.cpp
@@ -13,13 +13,6 @@ using namespace std;
 using namespace cv;
 using namespace xfeatures2d;
 
-const int dis_slider_max = 100;
-const int block_slider_max = 255;
-int dis_curr = 28;
-int block_curr = 48 ;
-
-Mat K= (Mat1d(3, 3) << FX, 0, CX, 0, FY, CY, 0, 0, 1);
-
 //Left cam -> origin of coordinate system
 Mat currPosition = (Mat1d(1, 3) << 0, 0, 0);
 Mat currRotation = Mat::eye(3, 3, CV_64F);
@@ -27,9 +20,9 @@ Mat currRotation = Mat::eye(3, 3, CV_64F);
 //Right cam
 Mat relativeRCamPosition = (Mat1d(1,3) << 10, 0, 0);
 Mat relativeRCamRotation = Mat::eye(3,3,CV_64F);
+pcl::PointCloud<pcl::PointXYZRGB>::Ptr myPC;
 
-
-void processFirstStereo(TsukubaParser& tParser, Ptr<SURF> surf, Ptr<DescriptorMatcher> matcher)
+void processFirstStereo(TsukubaParser& tParser, Ptr<SURF>& surf, Ptr<DescriptorMatcher>& matcher)
 {
     Mat LGImage, RGImage;
     tParser.getNextStereoImages();
@@ -41,9 +34,9 @@ void processFirstStereo(TsukubaParser& tParser, Ptr<SURF> surf, Ptr<DescriptorMa
     cvHelpers::getFeatures(surf, LGImage, kpLeft, descriptorLeft );
     cvHelpers::getFeatures(surf, RGImage, kpRight, descriptorRight );
     // TODO: rename inlier_points to filteredFeatures or something
-    vector<vector<Point2d> > inlier_points =  cvHelpers::filterPoints(matcher, kpLeft, descriptorLeft, kpRight, descriptorRight);
+    vector<vector<Point2f> > inlier_points =  cvHelpers::filterPoints(matcher, kpLeft, descriptorLeft, kpRight, descriptorRight);
     vector<Point3d> points_3d = cvHelpers::get3DPoints(inlier_points.at(0), inlier_points.at(1), currPosition, currRotation);
-    pcl::PointCloud<pcl::PointXYZRGB>::Ptr myPC = pclHelpers::Vec3DToPointCloudXYZRGB(points_3d, inlier_points.at(1), tParser.getLImage());
+    myPC = pclHelpers::Vec3DToPointCloudXYZRGB(points_3d, inlier_points.at(1), tParser.getLImage());
     pclHelpers::registerFirstPointCloud( myPC );
 }
 
@@ -62,8 +55,14 @@ int main(int argc, char** argv)
     Mat LGImage, RGImage;
     vector< vector<Point2f> > filtered_points;
     vector<Point2f> filtered_pointsLeft, filtered_pointsRight;
+    Mat outPos = (Mat1d(1, 3) << 0, 0, 0);
+    Mat outRot = Mat::eye(3, 3, CV_64F);
+    Mat descriptorLeft, descriptorRight;
+
     //TODO: add "Process first stereo image here". We have to set first image as frame of reference for all others
+    cout<< "ProcessFirstStereo\n";
     processFirstStereo(tParser, surf, matcher);
+    cout<< "Loop\n";
     while( tParser.getIter() <= NTSD_DB_SIZE)
     {
         tParser.getNextStereoImages();
@@ -71,19 +70,24 @@ int main(int argc, char** argv)
         cvtColor(tParser.getRImage(), RGImage, COLOR_BGRA2GRAY);
         //cout << LGImage.rows << " cols: " << LGImage.cols << endl;
         vector<KeyPoint> kpLeft,kpRight;
-        Mat descriptorLeft, descriptorRight;
         //Get features
         cvHelpers::getFeatures(surf, LGImage, kpLeft, descriptorLeft );
         cvHelpers::getFeatures(surf, RGImage, kpRight, descriptorRight );
         // TODO: rename inlier_points to filteredFeatures or something
-        vector<vector<Point2d> > inlier_points =  cvHelpers::filterPoints(matcher, kpLeft, descriptorLeft, kpRight, descriptorRight);
+        vector<vector<Point2f> > inlier_points =  cvHelpers::filterPoints(matcher, kpLeft, descriptorLeft, kpRight, descriptorRight);
         vector<Point3d> points_3d = cvHelpers::get3DPoints(inlier_points.at(0), inlier_points.at(1), currPosition, currRotation);
         //Get point cloud
-        pcl::PointCloud<pcl::PointXYZRGB>::Ptr myPC = pclHelpers::Vec3DToPointCloudXYZRGB(points_3d, inlier_points.at(1), tParser.getLImage());
+        myPC = pclHelpers::Vec3DToPointCloudXYZRGB(points_3d, inlier_points.at(1), tParser.getLImage());
         //Do ICP
-
+        pclHelpers::registerCurrentPointCloud(myPC);
         //Apply transform to currentPos and currentRot
+        Mat transform = pclHelpers::performICP();
 
+        Mat currPose = cvHelpers::convertTo4x4Pose(currRotation,currPosition);
+        Mat finalPose = currPose * transform;
+        cvHelpers::decompose4x4Pose(finalPose, outPos, outRot);
+        currPosition = outPos.clone();
+        currRotation = outRot.clone(); 
         if( tParser.showStereoImages() == 27)
         {
             break;
