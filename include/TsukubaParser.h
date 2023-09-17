f1b832b8dd31b93402b4dcb38caab18bfc1018f7
Rahul Anwardeen <ranwardeen@uhnder.com>
2023-09-18 00:09:11 +0530

    Add helper tsukuba functions

--- include/TsukubaParser.h
+++ include/TsukubaParser.h
@@ -13,10 +13,25 @@ class TsukubaParser{
         Mat getRImage();
         static Mat getCameraMatrix()
         {
-            return (Mat1d(3,3) << 615.f, 0.f, 320.f,        // fx, 0, cx
-                                    0.f, 615.f, 240.f,      // 0, fy, cy,
-                                    0,   0,     1);         // 0, 0 , 1
+            return (Mat1d(3,3) << 615.f, 0.f, 320.f,         // fx, 0, cx
+                                    0.f, 615.f, 240.f,       // 0, fy, cy,
+                                    0,   0,     1 );         // 0, 0 , 1
         };
+
+        static Mat getRightCamRelativeTransform()
+        {
+            return ( Mat1d(4, 4) << 1, 0, 0, 10,   //Baseline -> 10cm
+                                    0, 1, 0, 0,
+                                    0, 0, 1, 0,
+                                    0, 0, 0, 1);
+        }
+
+        /// @brief Returns rows and columns of the images in NTSD
+        /// @return vector<int> in the format {row, column}
+        static vector<int> getImageShape()
+        {
+            return vector<int>{480, 640};
+        }
 
         int showStereoImages();
         int getIter();
