cf7f81658778e3fc99c6b77199484e6d31e07397
Rahul Anwardeen <ranwardeen@uhnder.com>
2023-09-18 19:17:52 +0530

    Go back to double representation

--- include/cv-helpers.h
+++ include/cv-helpers.h
@@ -6,8 +6,8 @@
 
 namespace cvHelpers{
     void getFeatures( cv::Ptr<cv::xfeatures2d::SURF> surf, cv::Mat image, vector<cv::KeyPoint>& keyPoints, cv::Mat& descriptor  );
-    vector< vector<cv::Point2f> > filterPoints(cv::Ptr<cv::DescriptorMatcher>& matcher, vector<cv::KeyPoint> keyPoints1, cv::Mat& descriptor1, vector<cv::KeyPoint> keyPoints2, cv::Mat& descriptor2 );
-    vector<cv::Point3d> get3DPoints(vector<cv::Point2f> triangulation_points1, vector<cv::Point2f> triangulation_points2, cv::Mat& currentPos, cv::Mat& currentRot);
+    vector< vector<cv::Point2d> > filterPoints(cv::BFMatcher* matcher, vector<cv::KeyPoint> keyPoints1, cv::Mat& descriptor1, vector<cv::KeyPoint> keyPoints2, cv::Mat& descriptor2 );
+    vector<cv::Point3d> get3DPoints(vector<cv::Point2d> triangulation_points1, vector<cv::Point2d> triangulation_points2, cv::Mat& currentPos, cv::Mat& currentRot);
     cv::Mat convertTo3x4Pose(cv::Mat rotation, cv::Mat position);
     cv::Mat convertTo4x4Pose(cv::Mat rotation, cv::Mat position);
     void decompose4x4Pose(cv::Mat pose, cv::Mat& outRotation, cv::Mat& outPosition);
